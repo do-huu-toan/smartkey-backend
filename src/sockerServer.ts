9bb1390a9c4ee29c9af8fd3c4ec98872c6a50b7d
dhtoan <dohuutoannb@gmail.com>
2022-11-08 01:43:41 +0700

    feat: Update socketServer

--- src/sockerServer.ts
+++ src/sockerServer.ts
@@ -1,46 +1,93 @@
 import { Namespace, Server, Socket } from "socket.io";
-import memoryCache from "memory-cache"
+import memoryCache from "memory-cache";
+import { DbContext as _context } from "./entity/datasource";
+import { Devices } from "./entity/Devices";
+import * as jwt from "jsonwebtoken";
 class SocketServer {
   private io: Server;
-  private memCache = new memoryCache.Cache()
-  private ioUser: Namespace;
+  private memCache = new memoryCache.Cache();
   private ioDevice: Namespace;
   constructor(httpServer: any) {
-    this.io = new Server(httpServer, {});
-    this.ioDevice = this.io.of("/device")
-    this.ioUser = this.io.of("/user")
+    this.io = new Server(httpServer, {
+      cors: {
+        origin: "http://localhost:8080",
+      },
+    });
+    this.ioDevice = this.io.of("/device");
+    this.IOMiddleware();
     this.listen();
+    this.IODeviceMiddleware();
     this.listenIODevice();
-    this.listenIOUser();
+  }
+  public IOMiddleware() {
+    this.io.use(async function (socket, next) {
+      var token = socket.handshake.auth.token;
+      try {
+        const payload: any = jwt.verify(
+          token,
+          String(process.env.JWTSECRETKEY)
+        );
+        socket.handshake.query.userId = payload.id;
+        next();
+      } catch (error) {
+        next(new Error("Unauthorized"));
+      }
+    });
   }
   public listen(): void {
     console.log("Socket Server is start");
     this.io.on("connection", (socket) => {
-      this.memCache.put(`device/${socket.id}`, socket.id);
-      console.log(this.memCache.keys())
-      console.log("Co socket ket noi");
+      //Các thiết bị đang online
+      this.handleEmitDeviceOnline(socket);
+      this.handleControlDevice(socket);
+      socket.on("disconnect", () => {});
+    });
+  }
+  public IODeviceMiddleware() {
+    var self = this;
+    this.ioDevice.use(async function (socket, next) {
+      try {
+        var device = await _context.getRepository(Devices).findBy({
+          id: String(socket.handshake.query.token),
+        });
+        if (device.length > 0) {
+          self.memCache.put(
+            `device/${socket.handshake.query.token}/${device[0].userId}`,
+            socket.id
+          );
+          socket.handshake.query.userId = device[0].userId;
+          next();
+        } else next(new Error("Unauthorized"));
+      } catch (error) {
+        next(new Error("Unauthorized"));
+      }
+    });
+  }
+  public listenIODevice() {
+    this.ioDevice.on("connection", (socket) => {
       socket.on("disconnect", () => {
-        console.log("Co socket ngat ket noi");
-        this.memCache.del(`device/${socket.id}`);
-        console.log(this.memCache.keys())
+        this.memCache.del(
+          `device/${socket.handshake.query.token}/${socket.handshake.query.userId}`
+        );
       });
-      this.handleEvent(socket);
     });
   }
-  public listenIODevice(){
-    this.ioDevice.on("connection", (socket) => {
-      console.log("Có device kết nôi")
-    })
-  }
-  public listenIOUser(){
-    this.ioDevice.on("connection", (socket) => {
-      console.log("Có user kết nôi")
-    })
-  }
-  public handleEvent(socket: Socket): void {
-    socket.on("event/openlock", (data) => {
-      console.log(data);
+  
+  //Handle Emit
+  public handleEmitDeviceOnline(socket: any) {
+    const devicesOnline = this.memCache.keys().filter((key: any) => {
+      return (
+        key.startsWith("device/") &&
+        key.endsWith(`/${socket.handshake.query.userId}`)
+      );
     });
+    socket.emit("eventDeviceOnline", devicesOnline);
   }
+  public handleControlDevice(socket: any){
+    socket.on("control", function(data: object){
+      console.log(data)
+    })
+  }
+  //Handle On
 }
 export default SocketServer;
